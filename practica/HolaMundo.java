ba947514498ca7a0afaae29837a40ef99508ce73
Estudiantes <Estudiantes@S2dorada-PC1>
2014-09-04 17:00:22 -0500

    Multiplicacion

--- practica/HolaMundo.java
+++ practica/HolaMundo.java
@@ -1,7 +1,18 @@
+
+import java.util.Scanner;
+
 public class HolaMundo {
     
+        
     public static void main (String [ ] args){
-    System.out.println("Hola Mundo");
-    system.out.println("Hello!!");
+        Scanner sc =new Scanner(System.in); 
+ int n;
+ System.out.print("Introduce un número entero: ");
+        n = sc.nextInt();
+        System.out.println("Tabla del " + n);
+        for(int i = 1; i<=10; i++){
+             System.out.println(n + " * " + i + " = " + n*i);
+        }
+         
 }}
 
